5f39db840629b83dccf18629b48d9a74f21d699d
Mir Carvalho <deppbrazil@gmail.com>
2021-06-02 22:23:04 -0300

    :rocket: Add exercises 1, 2, 3, 4

--- Exercises/Form1.cs
+++ Exercises/Form1.cs
@@ -17,5 +17,47 @@ namespace Exercises
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Exercício 1
+            int idadeJoao = 20;
+            int idadeMaria = 30;
+            int idadeJulia = 10;
+            int media = (idadeJoao + idadeMaria + idadeJulia) / 3;
+
+            //MessageBox.Show("O valor da média de idade é " + media);
+            MessageBox.Show($"O valor da média de idade é {media}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Exercício 2
+
+            //int pi = 3.14;
+            // MessageBox.Show($"O valor de PI é {pi}");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Exercício 3
+            double pi = 3.14;
+            int piQuebrado = (int)pi;
+            MessageBox.Show("piQuebrado = " + piQuebrado);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Exercício 4
+            int a = 2;
+            int b = -10;
+            int c = 10;
+            double delta = b * b - 4 * a * c;
+
+            double a1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            double a2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+            MessageBox.Show($"a1 é {a1}");
+            MessageBox.Show($"a2 é {a2}");
+        }
     }
 }
